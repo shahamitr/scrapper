1eca5b1021fd866d4bb87babd34b2e5d56e9a79b
shahamitr <amit.r.shah.2005@gmail.com>
2022-06-10 14:11:26 +0530

    corrected product EANs

--- scrapper/spiders/gereedschapcentrum.py
+++ scrapper/spiders/gereedschapcentrum.py
@@ -1,22 +1,11 @@
 import scrapy
 import csv
-import pkgutil
 import time
 from random import randint
-import os
 
 #store ean from file
 
-list_ean = [8713515012285,
-8713515012308,
-8713515013558,
-8713515012469,
-0088381662710,
-5025536634265,
-5025536359496,
-5025536333779,
-5025536529653,
-5025536631035]
+list_ean = [8713515012285,8713515012308,8713515013558,8713515012469,0088381662710,5025536634265,5025536359496,5025536333779,5025536529653,5025536631035]
 
 class GereedschapcentrumSpider(scrapy.Spider):
     name = 'gereedschapcentrum'
@@ -30,12 +19,12 @@ class GereedschapcentrumSpider(scrapy.Spider):
     def parse(self, response,):
         time.sleep(randint(1, 2))
         data = {}
-        prijs_eu = str(response.xpath('//*[@id="maincontent"]/div[4]/div[1]/div/div[2]/ol/li/div/div[2]/div[2]/span[2]/span/span[2]/span/text()').getall())
+        prijs_eu = str(response.xpath('//*[@id="maincontent"]/div[4]/div[1]/div/div[2]/ol/li/div').getall())
         prijs_eu = prijs_eu.replace(",", ".")
         prijs_eu = prijs_eu.replace("[", "")
         prijs_eu = prijs_eu.replace("]", "")
         prijs_eu = prijs_eu.replace("'", "")
-        prijs_cent = str(response.xpath('//*[@id="maincontent"]/div[4]/div[1]/div/div[2]/ol/li/div/div[2]/div[2]/span[2]/span/span[2]/span/sup/text()').getall())
+        prijs_cent = str(response.xpath('//*[@id="maincontent"]/div[4]/div[1]/div/div[2]/ol/li/div/div').getall())
         prijs_cent = prijs_cent.replace("[", "")
         prijs_cent = prijs_cent.replace("]", "")
         prijs_cent = prijs_cent.replace("'", "")
@@ -45,11 +34,21 @@ class GereedschapcentrumSpider(scrapy.Spider):
             ean = ean.replace("[", "")
             ean = ean.replace("]", "")
             ean = ean.replace("'", "")
-            titel = str(response.xpath('//*[@id="maincontent"]/div[4]/div[1]/div/div[2]/ol/li/div/div[2]/strong/a/text()').getall())
+            
+            #title of the product
+            titel = str(response.xpath('//*[@class="product-item-link"]/font/font').getall())
             titel = titel.replace(']', '')
             titel = titel.replace('[', '')
             titel = titel.replace("'", "")
+            
+            #link of the product
+            link = str(response.xpath('//*[@class="product-item-link"]/@href').getall())
+            link = link.replace(']', '')
+            link = link.replace('[', '')
+            link = link.replace("'", "")
+
             data['Ean'] = ean
             data['Gereedschapcentrum'] = prijs
             data['Gereedschapcentrum-Titel'] = titel
+            data['Gereedschapcentrum-Link'] = link
             yield data
